64b8d13b95a33de3d9ffd7a829f10cba9b9ba3a9
Ash Julian <ash@electric-playground.com>
2020-08-30 13:53:05 -0300

    Optimizing mystery ship

--- commands/ship.js
+++ commands/ship.js
@@ -52,49 +52,35 @@ module.exports = {
     },
 
     getMysteryShip: function (params, receivedMessage) {
-        let user = {};
-        let kinName = {};
+        const maleKin = axios.get(`${helper.getOlApiString()}kin/male/1`);
+        const femaleKin = axios.get(`${helper.getOlApiString()}kin/female/1`);
 
-        if (params) {
-            if (/\buser=\b/i.test(params)) {
-                user = params.split("=");
-            }
-
-            if (/\bkin=\b/i.test(params)) {
-                kinName = params.split("=");
-            }
-        }
-
-        axios
-            .get(`${helper.getOlApiString()}kin/`)
-            .then(res => {
-                if (Object.entries(res.data).length !== 0) {
-                    let doe = res.data[Math.floor(Math.random() * res.data.length)];
-                    while (doe.gender_id !== 2) {
-                        doe = res.data[Math.floor(Math.random() * res.data.length)];
-                    }
-
-                    let buck = res.data[Math.floor(Math.random() * res.data.length)];
-                    while (buck.gender_id !== 1) {
-                        buck = res.data[Math.floor(Math.random() * res.data.length)];
-                    }
-
+        Promise.all([maleKin, femaleKin])
+            .then(function ([male, female]) {
+                if (
+                    male.status === 200 &&
+                    female.status === 200
+                ) {
                     receivedMessage.channel.send(
-                        doe.name +
+                        male.data.name +
                         " & " +
-                        buck.name +
+                        female.data.name +
                         "\n" +
-                        doe.current_image_url +
+                        male.data.current_image_url +
                         " " +
-                        buck.current_image_url
+                        female.data.current_image_url
                     );
                 } else {
-                    receivedMessage.channel.send(`I'm being lazy today, try again later.`);
+                    receivedMessage.channel.send(
+                        `I couldn't find those kin, would you like to try again?`
+                    );
                 }
             })
             .catch(err => {
                 console.log(err);
-                receivedMessage.channel.send(`I'm being lazy today, try again later.`);
+                receivedMessage.channel.send(
+                    `I couldn't find those kin, would you like to try again?`
+                );
             });
     }
 };
